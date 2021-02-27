f4debcc22520e9e7ce7db53fe7dca0b17ce552d4
Deddy Romnan <romnandeddy@gmail.com>
2021-02-27 22:41:46 +0700

    Add project files.

---
+++ Biodata/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Biodata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePickerDOB = new System.Windows.Forms.DateTimePicker();
+            this.checkBoxCCNA = new System.Windows.Forms.CheckBox();
+            this.checkBoxPenetrationTest = new System.Windows.Forms.CheckBox();
+            this.checkBoxNetworking = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxSex = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxOccupation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFullName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePickerDOB);
+            this.groupBox1.Controls.Add(this.checkBoxCCNA);
+            this.groupBox1.Controls.Add(this.checkBoxPenetrationTest);
+            this.groupBox1.Controls.Add(this.checkBoxNetworking);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBoxSex);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxOccupation);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxAddress);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxFullName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(486, 333);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // dateTimePickerDOB
+            // 
+            this.dateTimePickerDOB.Location = new System.Drawing.Point(139, 207);
+            this.dateTimePickerDOB.Name = "dateTimePickerDOB";
+            this.dateTimePickerDOB.Size = new System.Drawing.Size(336, 26);
+            this.dateTimePickerDOB.TabIndex = 32;
+            // 
+            // checkBoxCCNA
+            // 
+            this.checkBoxCCNA.AutoSize = true;
+            this.checkBoxCCNA.Location = new System.Drawing.Point(139, 299);
+            this.checkBoxCCNA.Name = "checkBoxCCNA";
+            this.checkBoxCCNA.Size = new System.Drawing.Size(79, 24);
+            this.checkBoxCCNA.TabIndex = 31;
+            this.checkBoxCCNA.Text = "CCNA";
+            this.checkBoxCCNA.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPenetrationTest
+            // 
+            this.checkBoxPenetrationTest.AutoSize = true;
+            this.checkBoxPenetrationTest.Location = new System.Drawing.Point(139, 269);
+            this.checkBoxPenetrationTest.Name = "checkBoxPenetrationTest";
+            this.checkBoxPenetrationTest.Size = new System.Drawing.Size(152, 24);
+            this.checkBoxPenetrationTest.TabIndex = 30;
+            this.checkBoxPenetrationTest.Text = "Penetration Test";
+            this.checkBoxPenetrationTest.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNetworking
+            // 
+            this.checkBoxNetworking.AutoSize = true;
+            this.checkBoxNetworking.Location = new System.Drawing.Point(139, 239);
+            this.checkBoxNetworking.Name = "checkBoxNetworking";
+            this.checkBoxNetworking.Size = new System.Drawing.Size(114, 24);
+            this.checkBoxNetworking.TabIndex = 29;
+            this.checkBoxNetworking.Text = "Networking";
+            this.checkBoxNetworking.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 20);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Skill(s)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Sex";
+            // 
+            // comboBoxSex
+            // 
+            this.comboBoxSex.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSex.FormattingEnabled = true;
+            this.comboBoxSex.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBoxSex.Location = new System.Drawing.Point(139, 167);
+            this.comboBoxSex.Name = "comboBoxSex";
+            this.comboBoxSex.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxSex.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Date of birth";
+            // 
+            // textBoxOccupation
+            // 
+            this.textBoxOccupation.Location = new System.Drawing.Point(139, 135);
+            this.textBoxOccupation.Name = "textBoxOccupation";
+            this.textBoxOccupation.Size = new System.Drawing.Size(336, 26);
+            this.textBoxOccupation.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Occupation";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(139, 51);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(336, 78);
+            this.textBoxAddress.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Address";
+            // 
+            // textBoxFullName
+            // 
+            this.textBoxFullName.Location = new System.Drawing.Point(139, 19);
+            this.textBoxFullName.Name = "textBoxFullName";
+            this.textBoxFullName.Size = new System.Drawing.Size(336, 26);
+            this.textBoxFullName.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Full Name";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(423, 590);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 34);
+            this.buttonOk.TabIndex = 18;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Enabled = false;
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 351);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(486, 233);
+            this.textBoxOutput.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 634);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Biodata";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxSex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxOccupation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFullName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.CheckBox checkBoxCCNA;
+        private System.Windows.Forms.CheckBox checkBoxPenetrationTest;
+        private System.Windows.Forms.CheckBox checkBoxNetworking;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOB;
+        private System.Windows.Forms.TextBox textBoxOutput;
+    }
+}
+
---
+++ Biodata/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Biodata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            string dob = dateTimePickerDOB.Value.ToString("dd MMMM yyyy");
+            int age = DateTime.Now.Year - dateTimePickerDOB.Value.Year;
+            if (dateTimePickerDOB.Value.Date > DateTime.Now.AddYears(age)) age--;
+
+            ArrayList skills = new ArrayList();
+            if (checkBoxNetworking.Checked) skills.Add("Networking");
+            if (checkBoxPenetrationTest.Checked) skills.Add("Penetration Test");
+            if (checkBoxCCNA.Checked) skills.Add("CCNA");
+
+            string allSkills = "";
+            for (int i = 0; i < skills.Count; i++)
+            {
+                if (i > 0) allSkills += ", ";
+                allSkills += skills[i];
+            }
+
+
+            string biodata =
+                "Full name : " + textBoxFullName.Text + "\r\n" +
+                "Address : " + textBoxAddress.Text + "\r\n" +
+                "Occupation : " + textBoxOccupation.Text + "\r\n" +
+                "Sex : " + comboBoxSex.Text + "\r\n" +
+                "Date of birth : " + dob + "\r\n" +
+                "Age : " + age + " years old\r\n" +
+                "Skills : " + allSkills;
+
+            textBoxOutput.Text = biodata;
+           
+        }
+    }
+}
